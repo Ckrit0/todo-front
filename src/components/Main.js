b422aa313e5f771a6fd2ccc2752596e09e01c811
ckrit <mito0525@naver.com>
2023-07-27 21:25:29 +0900

    메인페이지 CSS 적용

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,10 @@ function Main(){
 
     return(
         <div>
-            <p>환영합니다.</p>
-            <p>로고는 아직 없습니다.</p>
-            <p>로그인하면 일정이 나옴미다</p>
-            <p>아이디와 비밀번호는 대소문자를 따로 인식합니다.</p>
+            <p className="main_text">환영합니다.</p>
+            <p className="main_text">로고는 아직 없습니다.</p>
+            <p className="main_text">로그인하면 일정이 나옴미다</p>
+            <p className="main_text">아이디와 비밀번호는 대소문자를 따로 인식합니다.</p>
         </div>
     )
 }
